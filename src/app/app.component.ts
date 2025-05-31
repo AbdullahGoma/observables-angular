676bd2369ba7557af25a0e8bb908e2fcc8f99eff
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-31 19:40:19 +0300

    Signals vs Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,14 +8,36 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+  interval = signal(0);
+  doubleInterval = computed(() => this.interval() * 2);
+  
+  constructor() {
+    effect(() => {
+      console.log(`Clicked Button ${this.clickCount()} times`);
+    });
+  }
 
   ngOnInit() {
-    const subsctiption = interval(1000).pipe(
-      map((val) => val * 2)
-    ).subscribe({
-      next: (val) => console.log(val)      
-    });
+    // Siganl(Values in a container): Greate for manage app state
+    setInterval(() => {
+      // update some signal
+      this.interval.update((prevVal) => prevVal + 1);
+    }, 1000);
 
-    this.destroyRef.onDestroy(() => subsctiption.unsubscribe());
+    // Observable(values over time) is pipeline of values that emited over time
+    // Observable must subscribe
+    // Great for manage events and streamed data
+    // const subsctiption = interval(1000).pipe(
+    //   map((val) => val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val)
+    // });
+
+    // this.destroyRef.onDestroy(() => subsctiption.unsubscribe());
+  }
+ 
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
   }
 }
